9646b77468432d144270b7cd1eee216b7088fbd3
realityinspector <140459476+seanfromthepast@users.noreply.github.com>
2026-03-28 12:21:20 -0600

    main.py: delta-encoded sorted words (1722 -> 1600 bytes)

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from bz2 import decompress
 from base64 import b85decode
 S=decompress(b85decode(
-    b"LRx4!F+o`-Q(3CdxIq8_YJ-3P13&-Y^iTwT-1l#9Ugjv-uRTCC(4z@6(HNA_8lIX23PgG}Jwpvs@QO475E=xj`ic<K%6c^b&?MA|(KRzBiM0{GPg-~0j#<|C&mS`|XP#yD@{08u69P3%`;ieUY>JjG?b*Mcx2qO4Y)g;#E5u@ry2TO&Z^(>zcZuq9TKlFn9s7QNe=ky&cRG;!HMCCO%$ri}BWsIU<B2TN@gUJb;&)x?HJf1c_c>_SO?1*Yn&Q>zi`yJ_!B5GwLI@~=Dne4CiH4(NA3x!Ce)#*baQscJpKF$3Nd>%^F{cVB9El4qN)QJht-=gdJs}<=5@<>QYLpwg(C&6;tI~PveB<9Soa0*e=jsi+|G7BM{|{MF<?15GHiI3iWYEI@l{r4rx{dmOv<Om+OhiwhO_N6h^$hEpN9R=7nj|$lH1Qp1-J+XXnzdcI#|cS7dajNE2Af20ViV+BGV*c~MNy5jq@<)e_4jN{8X#o)Yp@#&Z%4<K`xmbte9U%<;Tce<Nf|0g0{!<UP|gsHY=cE1O0l4rCn{7CNW?X#U!C{dm7Q6FiyKixAS@{b`g{ja;MPFmGluqb!>vg}wHVTCshO>0OF1{|#@$Sg+7d1hg?&_%f|uHMT_QIpDMlOFCP@lbK$kA;*0)?^&tnm;{nu7c2t*`s%77LVOi`tJ^!wn`0Z~zS&)yrBLzfrJJ-qS1k3Rk$9r)f9!YL;0kh3F}S0T7>nG59FtFmCo;R2W=8BIY;CW0EWTyfpBn~0@YyGR$BVqkd6No#!Os>%c)5rU;8vb+RS+a63k-MDhxZd;_JooaxR5!7tMrU6Z`-z%M0Q||iJu?Uwa0g|9dlDQ-adSemi+b&r^@y>Y0MIw@tCjVNzkI0I@Gvd7^LEme$zo&uJxn&Tr#$+AVY{aRl!xt#iksEx6RqqYT>aRte@rq~RN2e-U0akc`kcO2wthJtFoVuDK_q>tYm0*RNc{6K&MzL@3_j|ow9o->)%)~@e>4l0x0zwE{LtfzM<y;kc6Pa&!wFt;VQpKNKL5aPpXO1^`&q1y?KKKo-C*1efi4wz?wbzVs&THh>*Hp#TX6;mF^L^FI^E;y;z}$JAKI3ji*r`lgR@R$9!5Wqp4IATUZHl`~Z0lHJRZO&nzbrODqmHxb^hJoPe0KHEj)~_PWt{S=7mmo)up6l}fN^lKVWoRb*~_(4JdW!>g6>GC3KA7r_Xr>"
+    b"LRx4!F+o`-Q&}Yvv}FJRG#P*Z1b6^H|KIRX1LSCy?Yeu;YBV)H5LEmHJVe??kZPZh5GtRFfsg}CMAXSl$*G_KN}r_&X_W!B4GjW9N_i%pQhG<IG!F*M=8LoUO<!j|<Js*I#Fz(>6Ei0VgNi*`&A|a8Y1+V#W^KGhJe;F>ZL)o_H`S_&l<5%G^v^KEobib6Qf1NB7QN4yT^oEbe=fyLzPBKeGgCVc$t?tlw!T9f21?6fo7Z4)lB<5C+oR%gDC;|VCIf8&oTEz1Pk8q7u`L!G>$g)=jQG7?&zA^Ypur&KxaM+3*9am(+4f-_G4vHYcRY7J=Mb@OnoETP$DZ|6pEmW1y~@P;rp*nUIADZYa|ckdol%&#P;BI-w*y;Nfy=*Jn1`HJ)0)#!=7JkBl@{E7hGm_|5`wBy?W+o_Y*(jO-rfd{P9h9+q|t<Ek+}hFFOxCQ@EHY;0!Jeh6UTc7%_Rcc`8He;<PTrl%XY&~m1UP>4ds2Z=M>*?0|OWV5HRG&4f{L|P1(JaZ%KYptoWMpRm+4U-OcykPkZ_I`Cl4piLj{!skiTpb*??!V1mGdj6K{J^`ceL2AnVw6o;#)!`}4NG!q0KyUlpxH%@;iTE{IGZNQk;T!ZB*{q)zu9wB5=x#~JNTddTsVQy5fJTj6AE{R@xZ8C}V;dXiym6}->tl<nnYWVt=x*!TML$bDVb(9mr0^}q_gchkE)N=<MJwn-3il|jpeciWAD90m-9J>@)+kJtwwoNBZL(k1w*FN-mygnH^>slmAN8?hwCuQiFYMWanInMn(rs=F+f~!SQ3?#ka^Na^Hs(v|kMIv3yg$9tyL=j5oB8{aRkqo^WF&Xizwl$$zVxtl~m4%oT&Am1q5f~Eb9rJ+^vOsQ8i@rIQLmC>sD5N*p$L2(s&kw`Tex`9<$BQxac&(Z3_TxZ|j98s-M1e*z7$+qNsW;WBWGD?C8jWnr*s*HkqK-!-n9NAKKw*ca>bYfqLCaCSK+}&-XGdr`_*NJ?hl0~sjqeD?>#4mEAJj)j#&1Aoab;l@LhKY#7|<bTCo++qTuVz4y}|)`%|<M>O<AlTZL`x@&4<3`)BIh@6yZWZluFT*"
 )).decode()
-unfold=lambda s=S:[p+x for b in s.split("^")for p,_,t in[b.partition("]")]for x in t.split("|")]
+def unfold(s=S):
+ W=[];p=''
+ for d in s.split(','):w=p[:int(d[0])]+d[1:];W+=[w];p=w
+ return W
 def t9(W=None):
  K="22233344455566677778889999";d={}
  for w in(W or unfold()):d.setdefault("".join(K[ord(c)-97]for c in w),[]).append(w)
